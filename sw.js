8564f52806e821c54ae0c3918215a9ea53e11529
gideonbrewer <gideon@gideonbrewer.com>
2026-08-12 12:03:49 -0400

    fix: refresh cached app shell

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE='wolfmaster-shell-v2';
+const CACHE='wolfmaster-shell-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET') return; event.respondWith(fetch(event.request).catch(()=>caches.match(event.request).then(res=>res||caches.match('./index.html'))));});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request).then(res=>res||caches.match('./index.html'))));
+});
